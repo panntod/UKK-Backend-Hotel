225bea1013e042faf94f1f3c075c99522448d231
panntod <pandhu.munjalindra@gmail.com>
2024-09-19 10:48:11 +0700

    feat: Add file upload functionality for user and room type photos

---
+++ controller/upload_foto_tipe.js
@@ -0,0 +1,47 @@
+const multer = require(`multer`);
+const path = require(`path`);
+const fs = require(`fs`);
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    const dir = "./public/foto_tipe_kamar";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(
+      null,
+      path.join(__dirname, `../public/foto_tipe_kamar`),
+      function (err, _) {
+        if (err) {
+          throw err;
+        }
+      },
+    );
+  },
+
+  filename: (_, file, cb) => {
+    let newFilename = `images-${Date.now()}${path.extname(file.originalname)}`;
+    cb(null, newFilename);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const acceptedType = [`image/jpg`, `image/jpeg`, `image/png`];
+    if (!acceptedType.includes(file.mimetype)) {
+      cb(null, false);
+      return cb(`Format File (${file.mimetype}) Tidak didukung`);
+    }
+
+    const filesize = req.headers[`content-length`];
+    const maxSize = 1 * 1024 * 1024;
+    if (filesize > maxSize) {
+      cb(null, false);
+      return cb(`File terlalu besar`);
+    }
+    cb(null, true);
+  },
+});
+
+module.exports = upload;
